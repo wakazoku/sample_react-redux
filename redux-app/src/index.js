cd0538ed6a03efeb464b3ff068d8102bb85dfc07
wakazoku <wakazoku@gmail.com>
2020-06-04 22:42:43 +0900

    redux with react

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = {
+  task: "",
+  tasks: [],
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// reducers
+function taskReducer(state = initialState, action) {
+  switch (action.type) {
+    case "INPUT_TASK":
+      return {
+        ...state,
+        task: action.payload.task,
+      };
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload.task],
+      };
+    default:
+      return state;
+  }
+}
+
+// storeを作成
+const store = createStore(taskReducer);
+
+// ActionCreators
+const inputTask = (task) => ({
+  type: "INPUT_TASK",
+  payload: {
+    task,
+  },
+});
+
+const addTask = (task) => ({
+  type: "ADD_TASK",
+  payload: {
+    task,
+  },
+});
+
+function TodoApp({ store }) {
+  const { task, tasks } = store.getState();
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => store.dispatch(inputTask(e.target.value))}
+      />
+      <input
+        type="button"
+        value="add"
+        onClick={() => store.dispatch(addTask(task))}
+      />
+      <ul>
+        {tasks.map((item, i) => {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function renderApp(store) {
+  console.log(store.getState());
+  render(<TodoApp store={store} />, document.getElementById("root"));
+}
+
+store.subscribe(() => renderApp(store));
+renderApp(store);
+
+export default TodoApp;
